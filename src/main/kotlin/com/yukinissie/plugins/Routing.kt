ab503a51bf2b74a26aff2f454647868ba38c62b8
yukinissie <bigtree2490@outlook.jp>
2023-06-17 10:53:14 +0900

    :sparkles: feat: サンプルコードの実装

--- src/main/kotlin/com/yukinissie/plugins/Routing.kt
+++ src/main/kotlin/com/yukinissie/plugins/Routing.kt
@@ -1,13 +1,23 @@
 package com.yukinissie.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        route("/") {
+            get {
+                call.respondText("Hi, this is yukinissie!", ContentType.Text.Html)
+            }
+
+            // return received text * 3
+            post("/") {
+                val text = call.receiveText()
+                call.respondText("$text-$text-$text")
+            }
         }
     }
 }
--- src/test/kotlin/com/yukinissie/ApplicationTest.kt
+++ src/test/kotlin/com/yukinissie/ApplicationTest.kt
@@ -1,5 +1,34 @@
 package com.yukinissie
 
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.mockk
 
-class ApplicationTest {
-}
+
+class ApplicationTest : FunSpec({
+
+    context("handleRequest") {
+        val app = App()
+        val event = APIGatewayProxyRequestEvent()
+        val context = mockk<Context>()
+
+        test("GET") {
+            event.httpMethod = "GET"
+            val response = app.handleRequest(event, context)
+
+            response.statusCode shouldBe 200
+            response.body shouldBe "Hi, this is yukinissie!"
+        }
+
+        test("POST") {
+            event.httpMethod = "POST"
+            event.body = "test"
+            val response = app.handleRequest(event, context)
+
+            response.statusCode shouldBe 200
+            response.body shouldBe "test-test-test"
+        }
+    }
+})
