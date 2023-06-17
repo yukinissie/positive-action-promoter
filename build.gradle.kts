9109c8f4706bcc6f6e9403a981dd8be5739d6552
Yuki Nishi <38881185+yukinissie@users.noreply.github.com>
2023-06-17 16:27:05 +0900

    :sparkles: feat: サンプルコードの実装 (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val kotest_version: String by project
+val mockk_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.22"
@@ -21,9 +25,35 @@ repositories {
 }
 
 dependencies {
+    implementation("com.amazonaws:aws-lambda-java-core:1.2.0")
+    implementation("com.amazonaws:aws-lambda-java-events:2.2.6")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-test-host:$ktor_version")
+    implementation("io.ktor:ktor-server-default-headers-jvm:$ktor_version")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotest_version")
+    testImplementation("io.kotest:kotest-assertions-core:$kotest_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("io.mockk:mockk:$mockk_version")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
+val buildZip by tasks.registering(Zip::class) {
+    from(tasks.named("compileKotlin"))
+    from(tasks.named("processResources"))
+    into("lib") {
+        from(configurations.runtimeClasspath)
+    }
+}
+
+tasks.named("build") {
+    dependsOn(buildZip)
 }
